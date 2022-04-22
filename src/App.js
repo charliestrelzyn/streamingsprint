97128f500b2f412503dba0f49308ffda5c710dd1
Charlie <charliestrelzyn@icloud.com>
2022-04-22 18:37:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import Navbar from './components/header/Navbar';
+// import Header from './components/header/Header';
+import Home from './pages/home/Home.js'
+import About from './pages/About';
+import Contact from './pages/contact/Contact.js';
+import SearchResults from './pages/search/SearchResults';
+import TitleDetails from './pages/titledetails/TitleDetails';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { StreamSources } from './pages/titledetails/streaminglist/StreamSources.js';
+import {ModeContext} from './context/ModeContext';
+import AllTitleDetails from './pages/titledetails/moretitledetails/AllTitleDetails.js';
+
 
 function App() {
+
+  const [darkMode,setDarkMode]=useState(false);
+
+  const toggleMode=()=>{
+    setDarkMode(!darkMode)
+    }
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ModeContext.Provider value={{darkMode,setDarkMode}}>
+      <Navbar toggleMode={toggleMode}/>
+
+      <Routes>
+        <Route path="/about" element={<About />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/" element={<Home />}/>
+        <Route path="/titledetails/:imdbID" element={<TitleDetails />}/>
+        <Route path="/titledetails/:imdbID" element={<StreamSources />}/>
+        <Route path="/titledetails/:imdbID" element={<AllTitleDetails />}/>
+      </Routes>
+      
+      {/* <Footer /> */}
+      </ModeContext.Provider>
     </div>
+    </Router>
+    
   );
 }
 
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -12,7 +12,6 @@ function Home() {
       <div class={darkMode ? "dark-home-container" : "home-container"}>
         <Main />
         <Logos/>
-        <p>Hello World</p>
         <footer>
             <div className={darkMode ? "dark-footer-container" : "footer-container"}>
               <a className={darkMode ? "reference-dark" : "reference"}href="https://www.watchmode.com/">Streaming Data Provided By WatchMode</a>
